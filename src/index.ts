c3283835a687ea67eac68a3bdc103e454c37b396
Clickcheng110110 <cl15213298081@gmail.com>
2024-04-28 22:40:40 +0800

    feat: init

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import RaydiumSwap from "./RaydiumSwap";
-import { Transaction, VersionedTransaction } from "@solana/web3.js";
+
 import "dotenv/config";
 import { swapConfig } from "./swapConfig"; // Import the configuration
 
@@ -7,7 +7,7 @@ import { swapConfig } from "./swapConfig"; // Import the configuration
  * Performs a token swap on the Raydium protocol.
  * Depending on the configuration, it can execute the swap or simulate it.
  */
-const swap = async () => {
+const poolInfo = async () => {
   /**
    * The RaydiumSwap instance for handling swaps.
    */
@@ -38,50 +38,7 @@ const swap = async () => {
     swapConfig.tokenAAddress,
     swapConfig.tokenBAddress
   );
-  console.log("Found pool info");
-
-  /**
-   * Prepare the swap transaction with the given parameters.
-   */
-  const tx = await raydiumSwap.getSwapTransaction(
-    swapConfig.tokenBAddress,
-    swapConfig.tokenAAmount,
-    poolInfo,
-    swapConfig.maxLamports,
-    swapConfig.useVersionedTransaction,
-    swapConfig.direction
-  );
-
-  /**
-   * Depending on the configuration, execute or simulate the swap.
-   */
-  if (swapConfig.executeSwap) {
-    /**
-     * Send the transaction to the network and log the transaction ID.
-     */
-    const txid = swapConfig.useVersionedTransaction
-      ? await raydiumSwap.sendVersionedTransaction(
-          tx as VersionedTransaction,
-          swapConfig.maxRetries
-        )
-      : await raydiumSwap.sendLegacyTransaction(
-          tx as Transaction,
-          swapConfig.maxRetries
-        );
-
-    console.log(`https://solscan.io/tx/${txid}`);
-  } else {
-    /**
-     * Simulate the transaction and log the result.
-     */
-    const simRes = swapConfig.useVersionedTransaction
-      ? await raydiumSwap.simulateVersionedTransaction(
-          tx as VersionedTransaction
-        )
-      : await raydiumSwap.simulateLegacyTransaction(tx as Transaction);
-
-    console.log(simRes);
-  }
+  console.log("Found pool info", poolInfo);
 };
 
-swap();
+poolInfo();
